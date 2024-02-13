275a7950f297569bc4eb76511cb3ef1bf72cd8de
Joseph Sweeney <joe.sweeney@trailofbits.com>
2024-02-13 10:32:53 -0500

    Add dependency instructions to the help text

--- cmd/verify.rb
+++ cmd/verify.rb
@@ -13,6 +13,11 @@ module Homebrew
         Verify the build provenance of bottles using GitHub's attestation tools.
         This is done by first fetching the given bottles, and then verifying
         their provenance.
+
+        Note that this command depends on the GitHub CLI and the gh-attestation extension:
+        https://github.com/github-early-access/gh-attestation?tab=readme-ov-file#installation
+
+        Follow the instructions there to install.
       EOS
       switch "--formula", "--formulae",
              description: "List only formulae, or treat all named arguments as formulae."
