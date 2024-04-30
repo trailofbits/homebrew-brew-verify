7b64de88693d98f04893d104ce55152e32f2d851
Joe Sweeney <joe.sweeney@trailofbits.com>
2024-04-30 12:46:11 -0400

    Use Upstream Attestation Library (#9)
    * Use upstream Attestion lib
    
    * Catch exception to give more informative error message
    
    * fixup to opoo

--- cmd/verify.rb
+++ cmd/verify.rb
@@ -4,8 +4,7 @@
 require "abstract_command"
 require "formula"
 require "date"
-
-require_relative "../lib/attestation"
+require "attestation"
 
 # The `Homebrew` namespace.
 module Homebrew
@@ -68,45 +67,11 @@ module Homebrew
                 next
               end
               bottle.fetch
-              result = Homebrew::Attestation.check_attestation(bottle, "Homebrew/homebrew-core")
-              if result[:verified]
-                if args.json?
-                  json_results.push(result[:data])
-                else
-                  ohai "Verified #{bottle.name} with tag #{bottle_tag}."
-                end
-              else
-                case result[:error]
-                when JSON::ParserError
-                  opoo "#{bottle.name} with tag #{bottle_tag} returned invalid json: #{result[:message]}"
-                  next
-                when Homebrew::Attestation::COMMAND_ERROR
-                  opoo "#{bottle.name} with tag #{bottle_tag} unverified, checking backfill signature."
-                  backup_result = Homebrew::Attestation.check_attestation(bottle, "trailofbits/homebrew-brew-verify")
-                  if backup_result[:verified]
-                    timestamp = backup_result.dig(:data, 0, "verificationResult", "verifiedTimestamps", 0,
-                                                  "timestamp")
-                    if timestamp.nil?
-                      opoo "Unable to verify #{bottle.name} with tag #{bottle_tag} " \
-                           "because json does not have signature timestamp in expected location."
-                      next
-                    end
-                    parsed_timestamp = DateTime.parse(timestamp)
-                    # Backfilled signatures all occured before this date so if we find
-                    # a backfilled signature after this date, we should disregard it.
-                    last_signature_date = DateTime.new(2024, 3, 14)
-                    if parsed_timestamp < last_signature_date
-                      if args.json?
-                        json_results.push(backup_result[:data])
-                      else
-                        ohai "Verified #{bottle.name} with tag #{bottle_tag} using backfill signature."
-                      end
-                    else
-                      opoo "Unable to verify #{bottle.name} with tag #{bottle_tag}. " \
-                           "Backfilled signature dated after last backfill date."
-                    end
-                  end
-                end
+              begin
+                attestation = Homebrew::Attestation.check_core_attestation bottle
+                json_results.push(attestation)
+              rescue Homebrew::Attestation::InvalidAttestationError => e
+                opoo "Unable to verify #{bottle.name} with tag #{bottle_tag} due to error:\n#{e}"
               end
             end
           end
